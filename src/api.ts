f2eb65cda3fc884aae544b5863149374ee006538
Leul Tewolde <leulwoldegabriel@gmail.com>
2024-12-29 14:35:16 -0600

    some changes

--- src/api.ts
+++ src/api.ts
@@ -2,8 +2,7 @@ import axios, { AxiosResponse } from 'axios';
 
 export async function fetchData(url: string): Promise<AxiosResponse<any>> {
     try {
-        const response = await axios.get(url);
-        return response;
+        return await axios.get(url);
     } catch (error) {
         throw new Error(`Failed to fetch data from ${url}: ${error}`);
     }
